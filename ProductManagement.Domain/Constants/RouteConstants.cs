1551dd174ce9a554adf2d39dfbd256af278f5972
Sakhawat Hossain <devsakhawat@outlook.com>
2022-11-01 13:38:32 +0600

    customer controller

--- ProductManagement.Domain/Constants/RouteConstants.cs
+++ ProductManagement.Domain/Constants/RouteConstants.cs
@@ -8,48 +8,21 @@ namespace ProductManagement.Domain.Constants
 {
    public static class RouteConstants
    {
-      public const string BaseRoute = "tuso-api";
+      public const string BaseRoute = "toner-api";
 
-      #region UserAccounts
-      public const string CreateUserAccount = "user-account";
+      #region Customer
+      public const string CreateCustomer = "customer";
 
-      public const string ReadUserAccounts = "user-accounts";
+      public const string ReadCustomers = "customers";
 
-      public const string ReadUserAccountByKey = "user-account/key/{key}";
+      public const string ReadCustomerByKey = "customer/key/{key}";
 
-      public const string ReadUserAccountByUsername = "user-account/{username}";
+      public const string ReadCustomerByCustomerName = "customer/{customerName}";
 
-      public const string UserDetails = "user-account/details/{key}";
+      public const string UpdateCustomer = "customer/{key}";
 
-      public const string UpdateUserAccount = "user-account/{key}";
-
-      public const string DeleteUserAccount = "user-account/{key}";
-
-      public const string UserLogin = "user-account/login";
-
-      public const string ChangedPassword = "user-account/change-password";
-
-      public const string RecoveryRequest = "user-account/recovery-request";
+      public const string DeleteCustomer = "customer/{key}";
       #endregion
-
-      #region ApplicationPermission
-      public const string CreateApplicationPermission = "application-permission";
-
-      public const string ReadApplicationPermissions = "application-permissions";
-
-      public const string ReadApplicationPermissionByKey = "application-permission/key/{OID}";
-
-      public const string ReadApplicationPermissionByRole = "application-permission/role/{RoleID}";
-
-      public const string ReadApplicationPermissionByModule = "application-permission/module/{ModuleID}";
-
-      public const string ReadApplicationPermission = "application-permission/key";
-
-      public const string UpdateApplicationPermission = "application-permission/{key}";
-
-      public const string DeleteApplicationPermission = "application-permission/{key}";
-      #endregion
-
 
 
    }
